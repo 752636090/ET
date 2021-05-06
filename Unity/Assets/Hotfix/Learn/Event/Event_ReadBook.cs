a20ee325702e9c56078943e52b0450aae18137df
752636090 <752636090@qq.com>
2021-05-06 22:57:03 +0800

    06 事件订阅与发布 完成

---
+++ Unity/Assets/Hotfix/Learn/Event/Event_ReadBook.cs
@@ -0,0 +1,29 @@
+﻿using ETModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETHotfix
+{
+    //[Event(EventIdType.ReadBook)]
+    //public class Event_ReadBook : AEvent
+    //{
+    //    public override void Run()
+    //    {
+    //        Log.Debug("小明读书");
+    //    }
+    //}
+
+    // 不能同时有带参和无参的
+
+    [Event(EventIdType.ReadBook)]
+    public class Event_ReadBookWithName : AEvent<string>
+    {
+        public override void Run(string bookName)
+        {
+            Log.Debug($"小明读{bookName}");
+        }
+    }
+}
