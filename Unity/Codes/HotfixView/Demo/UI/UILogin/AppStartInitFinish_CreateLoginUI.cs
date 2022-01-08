1f7d6c5074d4aa5725efab6a3e3463b140033b80
752636090 <752636090@qq.com>
2022-01-08 23:53:27 +0800

    放置入门 12 ET框架Protobuf通讯消息（ETTask部分）

--- Unity/Codes/HotfixView/Demo/UI/UILogin/AppStartInitFinish_CreateLoginUI.cs
+++ Unity/Codes/HotfixView/Demo/UI/UILogin/AppStartInitFinish_CreateLoginUI.cs
@@ -1,5 +1,7 @@
 ﻿
 
+using UnityEngine;
+
 namespace ET
 {
 	public class AppStartInitFinish_RemoveLoginUI: AEvent<EventType.AppStartInitFinish>
@@ -8,52 +10,58 @@ namespace ET
 		{
 			await UIHelper.Create(args.ZoneScene, UIType.UILogin);
 
-			#region Learn
-			// 放置第8节课会讲ZoneScene.AddChild和unitComponent.AddChildWithId的区别
-			//Computer computer = args.ZoneScene.AddChild<Computer>();
+            #region Learn
+            // 放置第8节课会讲ZoneScene.AddChild和unitComponent.AddChildWithId的区别
+            //Computer computer = args.ZoneScene.AddChild<Computer>();
 
-			////Game.EventSystem.Publish(new EventType.InstallComputer() { Computer = computer });
-			////Game.EventSystem.PublishAsync(new EventType.InstallComputer() { Computer = computer }).Coroutine();
-			//await Game.EventSystem.PublishAsync(new EventType.InstallComputer() { Computer = computer });
-			//computer.Start();
+            ////Game.EventSystem.Publish(new EventType.InstallComputer() { Computer = computer });
+            ////Game.EventSystem.PublishAsync(new EventType.InstallComputer() { Computer = computer }).Coroutine();
+            //await Game.EventSystem.PublishAsync(new EventType.InstallComputer() { Computer = computer });
+            //computer.Start();
 
-			////computer.AddComponent<PCCaseComponent>();
-			////computer.AddComponent<MonitorsComponent>();
-			////computer.AddComponent<KeyBoardComponent>();
-			////computer.AddComponent<MouseComponent>();
+            ////computer.AddComponent<PCCaseComponent>();
+            ////computer.AddComponent<MonitorsComponent>();
+            ////computer.AddComponent<KeyBoardComponent>();
+            ////computer.AddComponent<MouseComponent>();
 
-			////computer.Start();
+            ////computer.Start();
 
-			////await TimerComponent.Instance.WaitAsync(3000);
+            ////await TimerComponent.Instance.WaitAsync(3000);
 
-			////computer.Dispose();
+            ////computer.Dispose();
 
-			////UnitConfig config = UnitConfigCategory.Instance.Get(1001);
+            ////UnitConfig config = UnitConfigCategory.Instance.Get(1001);
 
-			////Log.Debug(config.Name);
+            ////Log.Debug(config.Name);
 
-			////System.Collections.Generic.Dictionary<int, UnitConfig> allUnitConfig = UnitConfigCategory.Instance.GetAll();
+            ////System.Collections.Generic.Dictionary<int, UnitConfig> allUnitConfig = UnitConfigCategory.Instance.GetAll();
 
-			////foreach (UnitConfig unitConfig in allUnitConfig.Values)
-			////         {
-			////	Log.Debug(unitConfig.Name);
-			////	Log.Debug(unitConfig.TestValue.ToString());
-			////         }
+            ////foreach (UnitConfig unitConfig in allUnitConfig.Values)
+            ////         {
+            ////	Log.Debug(unitConfig.Name);
+            ////	Log.Debug(unitConfig.TestValue.ToString());
+            ////         }
 
-			////UnitConfig heightConfig = UnitConfigCategory.Instance.GetUnitConfigByHeight(178);
+            ////UnitConfig heightConfig = UnitConfigCategory.Instance.GetUnitConfigByHeight(178);
 
-			////Log.Debug(heightConfig.Name);
+            ////Log.Debug(heightConfig.Name);
 
-			Log.Debug("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
 
-			//await this.TestAsync();
-			//this.TestAsync().Coroutine();
-			int result = await this.TestResultAsync();
-			Log.Debug(result.ToString());
 
-			Log.Debug("bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb");
-			#endregion
-		}
+            Log.Debug("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+
+            ////await this.TestAsync();
+            ////this.TestAsync().Coroutine();
+            //int result = await this.TestResultAsync();
+            //Log.Debug(result.ToString());
+
+            ETCancellationToken cancellationToken = new ETCancellationToken();
+			MoveToAsync(Vector3.zero, cancellationToken).Coroutine();
+            Log.Debug("bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb");
+            cancellationToken.Cancel();
+            // 笔记：TimerComponent中 ETTask<bool> tcs = ETTask<bool>.Create(true); 可以抹除第三方插件的回调，后续课程会讲
+            #endregion
+        }
 
 		#region Learn
 		public async ETTask TestAsync()
@@ -76,6 +84,20 @@ namespace ET
 
 			return 10;
 		}
+
+		public async ETTask MoveToAsync(Vector3 pos, ETCancellationToken cancellationToken)
+        {
+			Log.Debug("Move Start!!!");
+			bool ret = await TimerComponent.Instance.WaitAsync(3000, cancellationToken);
+			if (ret)
+            {
+				Log.Debug("Move Over!!!");
+            }
+            else
+            {
+				Log.Debug("Move Stop!!!");
+            }
+        }
         #endregion
     }
 }
