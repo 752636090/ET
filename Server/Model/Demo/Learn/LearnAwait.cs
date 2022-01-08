37af54626f2d8cc581c08fd099b33da163e4d3af
752636090 <752636090@qq.com>
2022-01-08 23:36:33 +0800

    放置入门 11 ET框架ETTask异步编程

---
+++ Server/Model/Demo/Learn/LearnAwait.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace ET
+{
+    class LearnAwait
+    {
+        #region 一
+        public void Invoke()
+        {
+            Log.Debug("1111111111");
+            //ReadFile(); // 1
+            ReadFileAsync(); // 2
+            Log.Debug("2222222222");
+        }
+
+        /// <summary>
+        /// 1
+        /// </summary>
+        public void ReadFile()
+        {
+            string content = File.ReadAllText("test.txt");
+            Log.Debug(content);
+        }
+
+        /// <summary>
+        /// 2
+        /// </summary>
+        public async void ReadFileAsync()
+        {
+            string content = await File.ReadAllTextAsync("test.txt");
+            Log.Debug(content);
+        }
+
+        /// <summary>
+        /// 2编译后的样子
+        /// </summary>
+        public void ReadFileAsyncCLR()
+        {
+            var awaiter = File.ReadAllTextAsync("test.txt").GetAwaiter();
+            awaiter.OnCompleted(() =>
+            {
+                string content = awaiter.GetResult();
+                Log.Debug(content);
+            });
+        }
+        #endregion
+
+        #region 二
+        async static void AsyncTestFunction()
+        {
+            await Task.Delay(1);
+            for (int i = 0; i < 10; i++)
+            {
+                Log.Debug($"AsyncFunction:i={i}");
+            }
+        }
+
+        public static void Main()
+        {
+            AsyncTestFunction();
+
+            for (int i = 0; i < 10; i++)
+            {
+                Log.Debug($"Main:i={i}");
+            }
+            Console.ReadLine();
+        }
+        #endregion
+    }
+}
--- Unity/Codes/HotfixView/Demo/UI/UILogin/AppStartInitFinish_CreateLoginUI.cs
+++ Unity/Codes/HotfixView/Demo/UI/UILogin/AppStartInitFinish_CreateLoginUI.cs
@@ -10,40 +10,72 @@ namespace ET
 
 			#region Learn
 			// 放置第8节课会讲ZoneScene.AddChild和unitComponent.AddChildWithId的区别
-			Computer computer = args.ZoneScene.AddChild<Computer>();
+			//Computer computer = args.ZoneScene.AddChild<Computer>();
 
-			//Game.EventSystem.Publish(new EventType.InstallComputer() { Computer = computer });
-			//Game.EventSystem.PublishAsync(new EventType.InstallComputer() { Computer = computer }).Coroutine();
-			await Game.EventSystem.PublishAsync(new EventType.InstallComputer() { Computer = computer });
-			computer.Start();
-
-			//computer.AddComponent<PCCaseComponent>();
-			//computer.AddComponent<MonitorsComponent>();
-			//computer.AddComponent<KeyBoardComponent>();
-			//computer.AddComponent<MouseComponent>();
-
+			////Game.EventSystem.Publish(new EventType.InstallComputer() { Computer = computer });
+			////Game.EventSystem.PublishAsync(new EventType.InstallComputer() { Computer = computer }).Coroutine();
+			//await Game.EventSystem.PublishAsync(new EventType.InstallComputer() { Computer = computer });
 			//computer.Start();
 
-			//await TimerComponent.Instance.WaitAsync(3000);
+			////computer.AddComponent<PCCaseComponent>();
+			////computer.AddComponent<MonitorsComponent>();
+			////computer.AddComponent<KeyBoardComponent>();
+			////computer.AddComponent<MouseComponent>();
 
-			//computer.Dispose();
+			////computer.Start();
 
-			//UnitConfig config = UnitConfigCategory.Instance.Get(1001);
+			////await TimerComponent.Instance.WaitAsync(3000);
 
-			//Log.Debug(config.Name);
+			////computer.Dispose();
 
-			//System.Collections.Generic.Dictionary<int, UnitConfig> allUnitConfig = UnitConfigCategory.Instance.GetAll();
+			////UnitConfig config = UnitConfigCategory.Instance.Get(1001);
 
-			//foreach (UnitConfig unitConfig in allUnitConfig.Values)
-   //         {
-			//	Log.Debug(unitConfig.Name);
-			//	Log.Debug(unitConfig.TestValue.ToString());
-   //         }
+			////Log.Debug(config.Name);
 
-			//UnitConfig heightConfig = UnitConfigCategory.Instance.GetUnitConfigByHeight(178);
+			////System.Collections.Generic.Dictionary<int, UnitConfig> allUnitConfig = UnitConfigCategory.Instance.GetAll();
 
-			//Log.Debug(heightConfig.Name);
-            #endregion
-        }
-	}
+			////foreach (UnitConfig unitConfig in allUnitConfig.Values)
+			////         {
+			////	Log.Debug(unitConfig.Name);
+			////	Log.Debug(unitConfig.TestValue.ToString());
+			////         }
+
+			////UnitConfig heightConfig = UnitConfigCategory.Instance.GetUnitConfigByHeight(178);
+
+			////Log.Debug(heightConfig.Name);
+
+			Log.Debug("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+
+			//await this.TestAsync();
+			//this.TestAsync().Coroutine();
+			int result = await this.TestResultAsync();
+			Log.Debug(result.ToString());
+
+			Log.Debug("bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb");
+			#endregion
+		}
+
+		#region Learn
+		public async ETTask TestAsync()
+        {
+			Log.Debug("1111111111111111111111111111111");
+			await TimerComponent.Instance.WaitAsync(2000);
+			Log.Debug("2222222222222222222222222222222");
+			//await ETTask.CompletedTask; // 用了CompletedTask就可以认为是同步
+		}
+
+		/// <summary>
+		/// 返回结果的异步方法肯定不能用Coroutine
+		/// </summary>
+		/// <returns></returns>
+		public async ETTask<int> TestResultAsync()
+        {
+			Log.Debug("1111111111111111111111111111111");
+			await TimerComponent.Instance.WaitAsync(2000);
+			Log.Debug("2222222222222222222222222222222");
+
+			return 10;
+		}
+        #endregion
+    }
 }
