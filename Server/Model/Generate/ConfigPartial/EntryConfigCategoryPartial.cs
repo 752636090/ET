f6e0e1827fc66c1df4902d3d7b90f6a8875fc111
752636090 <752636090@qq.com>
2022-04-30 17:57:21 +0800

    放置进阶 21 背包系统（五）  （服务器生成装备属性词条）

---
+++ Server/Model/Generate/ConfigPartial/EntryConfigCategoryPartial.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ET
+{
+    public partial class EntryConfigCategory
+    {
+        private Dictionary<int, MultiMap<int, EntryConfig>> EntryConfigDict = new Dictionary<int, MultiMap<int, EntryConfig>>();
+
+        public override void AfterEndInit()
+        {
+            base.AfterEndInit();
+
+            foreach (EntryConfig config in this.dict.Values)
+            {
+                if (!this.EntryConfigDict.ContainsKey(config.EntryType))
+                {
+                    this.EntryConfigDict.Add(config.EntryType, new MultiMap<int, EntryConfig>());
+                }
+                this.EntryConfigDict[config.EntryType].Add(config.EntryLevel, config);
+            }
+        }
+
+        public EntryConfig GetRandomEntryConfigByLevel(int entryType, int level)
+        {
+            if (!this.EntryConfigDict.ContainsKey(entryType))
+            {
+                return null;
+            }
+
+            MultiMap<int, EntryConfig> entryConfigMap = this.EntryConfigDict[entryType];
+            if (!entryConfigMap.ContainsKey(level))
+            {
+                return null;
+            }
+
+            List<EntryConfig> configList = entryConfigMap[level];
+            int index = RandomHelper.RandomNumber(0, configList.Count);
+            return configList[index];
+        }
+    }
+}
---
+++ Unity/Codes/Model/Example/ExampleIdleGame/Item/AttributeEntry.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+#if SERVER
+using MongoDB.Bson.Serialization.Attributes;
+#endif
+
+namespace ET
+{
+    public enum EntryType
+    {
+        Common = 1, // 普通词条
+        Special = 2, // 特殊词条
+    }
+
+#if SERVER
+    public class AttributeEntry : Entity, IAwake, IDestroy, ISerializeToEntity
+#else
+    public class AttributeEntry : Entity, IAwake, IDestroy
+#endif
+    {
+        /// <summary>
+        /// 词条数值属性类型
+        /// </summary>
+        public int Key;
+
+        /// <summary>
+        /// 词条数值属性值
+        /// </summary>
+        public long Value;
+
+        /// <summary>
+        /// 词条类型
+        /// </summary>
+        public EntryType Type;
+    }
+}
